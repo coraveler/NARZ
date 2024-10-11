af3782de8e3ff2688c00fddd0d0fd499cd58a9e0
Gunzzal <rkdrjsgks1508@gmail.com>
2024-10-11 16:20:06 +0900

    지역게시글 만드는 중

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Footer from "./layout/footer/Footer";
 import TravelHeader from "./layout/header/TravelHeader";
 import HomePage from "./pages/HomePage";
+import LocalBoard from "./pages/LocalBoard";
 
 function Header() {
   return <TravelHeader />;
@@ -14,6 +15,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/localboard" element={<LocalBoard />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
