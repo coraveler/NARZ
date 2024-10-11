f750216f96eeb636d91ce0afa2f51df322a3e500
siwooo2 <siu147@naver.com>
2024-10-11 15:22:41 +0900

    add calendar

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,16 @@
+<<<<<<< Updated upstream
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Footer from './layout/footer/Footer';
 import TravelHeader from './layout/header/TravelHeader';
+=======
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./layout/footer/Footer";
+import TravelHeader from "./layout/header/TravelHeader";
+import Calendar from "./pages/Calendar";
+import HomePage from "./pages/HomePage";
+>>>>>>> Stashed changes
 
 function Header() {
   return (
@@ -12,8 +21,17 @@ function Header() {
 function App() {
   return (
     <BrowserRouter>
+<<<<<<< Updated upstream
      <Header/>
      <Footer/>
+=======
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/calendar" element={<Calendar/>}/>
+      </Routes>
+      <Footer />
+>>>>>>> Stashed changes
     </BrowserRouter>
   );
 }
---
+++ front/src/pages/Calendar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Calendar() {
+  return (
+    <div>
+        달력 페이지
+    </div>
+  );
+}
+
+export default Calendar;
