20cdfe8c987c140f954f04e1907cb779a602a459
siwooo2 <siu147@naver.com>
2024-10-14 16:09:50 +0900

    backend setting

---
+++ backend/back/src/main/java/com/kdt_final/back/schedule/dao/ScheduleMapper.java
@@ -0,0 +1,9 @@
+package com.kdt_final.back.schedule.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class ScheduleMapper {
+    
+    
+}
---
+++ backend/back/src/main/java/com/kdt_final/back/schedule/service/ScheduleService.java
@@ -0,0 +1,9 @@
+package com.kdt_final.back.schedule.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduleService {
+    
+    
+}
