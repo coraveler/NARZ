48b32c9078a21782a142816b5310395fdb3504c6
Gunzzal <rkdrjsgks1508@gmail.com>
2024-10-14 11:33:23 +0900

    쿠폰_상점 변경, 지도 프로토타입 삽입

--- front/src/pages/CalendarPage.jsx
+++ front/src/pages/CalendarPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import MapOverlay from '../Includes/personalPage/MapOverlay';
 
 function CalendarPage() {
   return (
     <div>
         달력 홈페이지
+        <MapOverlay/>
     </div>
   );
 }
