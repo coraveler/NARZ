dc7781512f8970dd8fab485422050302640ae3d5
Gunzzal <rkdrjsgks1508@gmail.com>
2024-10-14 14:41:20 +0900

    ..

--- front/src/pages/CalendarPage.jsx
+++ front/src/pages/CalendarPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import MapOverlay from '../Includes/personalPage/MapOverlay';
 
 function CalendarPage() {
   return (
     <div>
         달력 홈페이지
-        <MapOverlay/>
     </div>
   );
 }
