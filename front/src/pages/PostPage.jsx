a7dce64f3add835420056e7370f80b05563caed4
Gunzzal <rkdrjsgks1508@gmail.com>
2024-10-15 15:32:00 +0900

    게시글 페이지

--- front/src/pages/PostPage.jsx
+++ front/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../css/PostPage.module.css';
 import ProfileInfo from '../Includes/common/ProfileInfo';
+import Comment from '../Includes/comment/Comment';
 
 const PostPage = () => {
 
@@ -22,6 +23,8 @@ const PostPage = () => {
                     </button>
                 </div>
             </section>
+            <hr style={{width:"70%"}}/>
+            <div align="center">
             <main className={styles.container}>
                 <section className={styles.productWrapper}>
                     <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/24910718777b542f7ef16fba09ae33c14a591dbd32af92b4c40c8f9a004c54b5?placeholderIfAbsent=true&apiKey=c7f1d91a917e4e2ba5370da6919a77db" className={styles.ratingIcon} alt="" />
@@ -34,15 +37,21 @@ const PostPage = () => {
                             {ratingData.map((item, index) => (
                                 <React.Fragment key={index}>
                                     <img loading="lazy" src={item.icon} className={styles.ratingIcon} alt={`Rating icon ${index + 1}`} />
-                                    <div className={styles.ratingValue}>{item.value}</div>
+                                    <div className={styles.ratingValue}>{item.value}</div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                                 </React.Fragment>
                             ))}
                         </div>
                         
                     </div>
                 </section>
+                <hr style={{width:"70%"}}/>
                 <section className={styles.contentSection}>내용</section>
             </main>
+            
+            <hr style={{width:"70%"}}/><Comment/>  {/* 반복 */}
+            
+            
+            </div>
         </div>
     );
 };
