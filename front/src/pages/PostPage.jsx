b5cddb4e155001c5e6050de0482ae237a64018cf
Gunzzal <rkdrjsgks1508@gmail.com>
2024-10-22 16:27:01 +0900

    ..

--- front/src/pages/PostPage.jsx
+++ front/src/pages/PostPage.jsx
@@ -5,11 +5,13 @@ import Comment from '../Includes/comment/Comment';
 import api from '../api/axios';
 import { useParams } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 const PostPage = () => {
     const { postId } = useParams();
     const [post, setPost] = useState({});
     const [postImgUrl, setPostImgUrl] = useState([]);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         // 예: API 호출하여 postId에 해당하는 포스트 데이터 가져오기
@@ -25,17 +27,17 @@ const PostPage = () => {
             // console.log("debug >>> response, ", response.data.comments);
             // setComments(response.data.comments);
         } catch (err) {
+            setError('Failed to load images');
             console.log(err);
         }
     };
 
-    const getPostImages = async (psotId) => {
+    const getPostImages = async (postId) => {
         try {
             const response = await api.get(`post/view/images/${postId}`);
             console.log("debug >>> response imgUrls >>>>>>>>>>>>>>>> ", response.data);
             setPostImgUrl(response.data);
-            // console.log("debug >>> response, ", response.data.comments);
-            // setComments(response.data.comments);
+
         } catch (err) {
             console.log(err);
         }
@@ -63,36 +65,19 @@ const PostPage = () => {
             <div align="center">
                 <main className={styles.container}>
                     <section className={styles.productWrapper}>
-                        {/* <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/24910718777b542f7ef16fba09ae33c14a591dbd32af92b4c40c8f9a004c54b5?placeholderIfAbsent=true&apiKey=c7f1d91a917e4e2ba5370da6919a77db" className={styles.ratingIcon} alt="" />
-                    <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/763c2adfc1e60cef4d55fd1aa38ea204b0495acfb56a7000fa4043bfd35c059c?placeholderIfAbsent=true&apiKey=c7f1d91a917e4e2ba5370da6919a77db" className={styles.productImage} alt="Product display" />
-                    <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/9578e85bd6ab614ca5d75da456338ed48b65ecab1d9b85a0ce643ba390c2bff2?placeholderIfAbsent=true&apiKey=c7f1d91a917e4e2ba5370da6919a77db" className={styles.ratingIcon} alt="" /> */}
-                        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-                            <div className="carousel-indicators">
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                            </div>
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
-                                    <img src="..." className="d-block w-100" alt="..."/>
+                    {error ? (<div>{error}</div>):(
+                        <Carousel>
+                            <Carousel.Item>
+                                <div className='slidercontents'>
+                                <img src={postImgUrl[0].imagePath} class="d-block w-100" alt="..."/>
                                 </div>
-                                <div className="carousel-item">
-                                    <img src="..." className="d-block w-100" alt="..."/>
-                                </div>
-                                <div className="carousel-item">
-                                    <img src="..." className="d-block w-100" alt="..."/>
-                                </div>
-                            </div>
-                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Previous</span>
-                            </button>
-                            <button className="carousel-control-next" type="button" data-bs-targetclassname="#carouselExampleIndicators" data-bs-slide="next">
-                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Next</span>
-                            </button>
-                        </div>
+                            </Carousel.Item>
+                            
+                        </Carousel>
+                       ) }
                     </section>
+
+
                     <section className={styles.ratingSection}>
                         <div className={styles.ratingWrapper}>
                             <div className={styles.ratingBar}>
