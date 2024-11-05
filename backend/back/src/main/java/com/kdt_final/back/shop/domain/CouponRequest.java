5ff2993ac984aa9fa761847e059f6f5c2423c397
yoonseo1265 <yoonseo1265@gmail.com>
2024-11-05 15:45:48 +0900

    쿠폰 수정

--- backend/back/src/main/java/com/kdt_final/back/shop/domain/CouponRequest.java
+++ backend/back/src/main/java/com/kdt_final/back/shop/domain/CouponRequest.java
@@ -1,22 +1,9 @@
 package com.kdt_final.back.shop.domain;
 
+import lombok.Data;
+
+@Data
 public class CouponRequest {
     private String couponCode;
-    private int userId;
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getCouponCode() {
-        return couponCode;
-    }
-
-    public void setCouponCode(String couponCode) {
-        this.couponCode = couponCode;
-    }
+    private int userId; 
 }
