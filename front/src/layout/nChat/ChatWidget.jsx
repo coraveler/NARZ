ade6a5dd9169ad0e38f7dc8714b5f3d443d1ab28
Gunzzal <rkdrjsgks1508@gmail.com>
2024-11-21 17:29:48 +0900

    채팅 실시간

--- front/src/layout/nChat/ChatWidget.jsx
+++ front/src/layout/nChat/ChatWidget.jsx
@@ -8,10 +8,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ChatRoomList from "./ChatPage/ChatRoomList";
 import api from "../../api/axios";
 
-const ChatWidget = ({ nc, loginId, recipientId, projectId, apiKey, isChatOpen, toggleChatWindow, openFromButton, channel, channels, openChatWindow, handleChatChange}) => {
+const ChatWidget = ({ nc, loginId, recipientId, projectId, apiKey, isChatOpen, toggleChatWindow, openFromButton, channel, channels, openChatWindow, handleChatChange, totalUnread}) => {
   const [activeTab, setActiveTab] = useState("friends");
   const [messages, setMessages] = useState([]);
-  const[totalUnread, setTotalUnread] = useState(0);
+  // const[totalUnread, setTotalUnread] = useState(0);
   const [state, setState] = useState(null);
 
 
@@ -75,19 +75,25 @@ const ChatWidget = ({ nc, loginId, recipientId, projectId, apiKey, isChatOpen, t
     toggleChatWindow(true); 
   };
 
-  const getTotalUnread = async() => {
-    try{
-      const response = await api.get(`chat/getTotalUnread/${loginId}`);
-      // console.log(response.data);
-      setTotalUnread(response.data);
-    }catch(error){
-      console.log(error);
-    }
-  }
-
+  // const getTotalUnread = async() => {
+  //   try{
+  //     const response = await api.get(`chat/getTotalUnread/${loginId}`);
+  //     console.log(response.data);
+  //     setTotalUnread(response.data);
+  //   }catch(error){
+  //     console.log(error);
+  //   }
+  // }
   useEffect(() => {
-    getTotalUnread();
-  },[channel,channels])
+    console.log(channels);
+    console.log(totalUnread)
+  },[totalUnread])
+
+
+  // useEffect(() => {
+  //   getTotalUnread();
+  // },[channel])
+
 
 
   return (
